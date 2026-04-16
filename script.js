c410ceab2af732f5f1222e3ef7affaa15f81b7ca
jorgsalazarp <jorg.salazarp@duocuc.cl>
2026-04-16 15:05:40 -0400

    Cambios a proyectos.html, añadido de css y js

--- script.js
+++ script.js
@@ -23,3 +23,35 @@ function escribirTexto() {
 
 // Iniciamos la función al cargar la página
 window.onload = escribirTexto;
+
+// ===========================================================
+// =========================================================== Borrar si es necesario
+// ===========================================================
+
+//proyectos.html
+document.addEventListener("DOMContentLoaded", () => {
+    const filterBtns = document.querySelectorAll('.filtros-btn');
+    const projectCards = document.querySelectorAll('.carta-proyecto');
+
+    filterBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            //Actualiza estilo del botón activo
+            filterBtns.forEach(b => b.classList.remove('active'));
+            btn.classList.add('active');
+
+            //Obtener categoría seleccionada
+            const filterValue = btn.getAttribute('data-filter');
+
+            //Filtrar las tarjetas
+            projectCards.forEach(card => {
+                const cardCategory = card.getAttribute('data-category');
+                
+                if (filterValue === 'all' || filterValue === cardCategory) {
+                    card.classList.remove('hidden');
+                } else {
+                    card.classList.add('hidden');
+                }
+            });
+        });
+    });
+});
